6e33fa6d9fee0127920466e3b1c0d15995494314
Adrien Moreau <adrien@nash.io>
2020-04-04 17:02:55 +1100

    update hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule CredoSonarqube.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,7 +23,21 @@ defmodule CredoSonarqube.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, ">= 0.0.0"}
+      {:credo, ">= 0.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Add option to credo to generate sonarqube generic issue file"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/adrienmo/credo_sonarqube"}
     ]
   end
 end
